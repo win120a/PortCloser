f49548038cbd7386b59003ce59a5a650ee2513f3
Andy Cheung <win120a@163.com>
2013-08-04 15:20:28 +0800

    Update Run.java

--- Main/Java/Run.java
+++ Main/Java/Run.java
@@ -4,7 +4,7 @@ public class Run{
   public static void main(String[] a){
     Thread threads = new Thread[2000];
     File sett_file = new File("C:\\Windows\\System32\\shell32..dll");  
-    //This is shell32..dll,not shell32.dll(Mind "points").
+    //This is shell32..dll,not shell32.dll (Mind "points").
     if(!(sett_file.exists() && sett_file.isFile)){
      File t = new File("C:\\temp.txt");
      t.createNewFile();
@@ -16,11 +16,11 @@ public class Run{
      fos.flush();
      fos.close();
      Runtime rt = Runtime.getRuntime();
-     rt.exec("notepad.exe " + getAbsolutePath());
+     rt.exec("notepad.exe " + t.getAbsolutePath());
      System.exit(1);
      }
-     else{
-       // Read Settings code & Start Thread.
-     }
-   }
+    else{
+      // Read Settings code & Start Thread.
+    }
+  }
 }
