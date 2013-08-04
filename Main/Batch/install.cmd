dd15633725e1a693135954a88fafe1be43e28d17
Andy Cheung <win120a@163.com>
2013-08-04 15:53:11 +0800

    Update install.cmd

--- Main/Batch/install.cmd
+++ Main/Batch/install.cmd
@@ -3,6 +3,10 @@ echo COPYING FILES...
 Rem This Folder I will not create here,because this is JRE Files.
 xcopy RT "C:\PC\run_engine" /y /e /q
 copy uninstall.cmd C:\PC\uninstall.cmd
-xcopy Java "C:\\PC\\run_engine\\run_engine" /y /e /q
-
-Rem Later...
+xcopy Java "C:\PC\run_engine\engine" /y /e /q
+xcopy Setter "C:\PC\Setter" /y /e /q
+echo Registering AutoRun...
+reg import Registry\install_autorun.reg
+echo Are you need run the setter?
+set /p c=.........[ ] (Yes/No)
+if "%c%" == "Yes" 
