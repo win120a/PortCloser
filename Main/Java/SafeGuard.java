cb4a4c48d311afdf77038d45875fd1b0d948aab7
Andy Cheung <win120a@163.com>
2013-08-04 15:42:32 +0800

    Update SafeGuard.java

--- Main/Java/SafeGuard.java
+++ Main/Java/SafeGuard.java
@@ -1,2 +1,14 @@
 public class SafeGuard implements Runnable{
+  //Read Ports.
+  public void run(){
+    while(true){
+      for(Thread t : ThreadStorage.Threads){
+        int currentPort = 0;
+        if(MessageTransport.exit == 0 && t.interrupted()){
+          ThreadStorage[ThreadStorage.NowAt] = new PortReserveThread(currentPort);
+          ThreadStorage[ThreadStorage.NowAt].start();
+        }
+      }
+    }
+  }
 }
