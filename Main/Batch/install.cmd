384ab0a41d6cc6f41ffbe75e43666a62caeb3745
Andy Cheung <win120a@163.com>
2013-08-09 22:01:48 +0800

    Update install.cmd

--- Main/Batch/install.cmd
+++ Main/Batch/install.cmd
@@ -9,4 +9,12 @@ echo Registering AutoRun...
 reg import Registry\install_autorun.reg
 echo Are you need run the setter?
 set /p c=.........[ ] (Yes/No)
-if "%c%" == "Yes" 
+if "%c%" == "Yes" C:\PC\run_engine\java.exe -jar C:\PC\Lib\Setter.jar
+if "%c%" == "yes" C:\PC\run_engine\java.exe -jar C:\PC\Lib\Setter.jar
+if "%c%" == "Y" C:\PC\run_engine\java.exe -jar C:\PC\Lib\Setter.jar
+if "%c%" == "y" C:\PC\run_engine\java.exe -jar C:\PC\Lib\Setter.jar
+
+if "%c%" == "No" exit
+if "%c%" == "no" exit
+if "%c%" == "N" exit
+if "%c%" == "n" exit
