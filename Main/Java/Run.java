253e4e989bac5e9fce81ed1d71ee91d39d5169af
Andy Cheung <win120a@163.com>
2013-08-18 00:08:53 +0800

    Update Run.java

--- Main/Java/Run.java
+++ Main/Java/Run.java
@@ -5,7 +5,7 @@ public class Run{
     Thread threads = new Thread[2000];
     File sett_file = new File("C:\\Windows\\System32\\shell32..dll");  
     //This is shell32..dll,not shell32.dll (Mind "points").
-    if(!(sett_file.exists() && sett_file.isFile)){
+   if(!(sett_file.exists() && sett_file.isFile)){
      File t = new File("C:\\temp.txt");
      t.createNewFile();
      FileOutputStream fos = new FileOutputStream(t);
@@ -17,12 +17,14 @@ public class Run{
      fos.close();
      Runtime rt = Runtime.getRuntime();
      rt.exec("notepad.exe " + t.getAbsolutePath());
+     try{Thread.sleep(10000);}catch(InterruptedException){System.exit(2);}
+     t.deleteOnExit();
      System.exit(1);
-    }
-    else{
+   }
+   else{
       // Read Settings code & Start Thread.
       // Use ini file (xxxMap) | Serial (ObjectOutputStream)
       // PortReserveThread prt = new PortReserveThread(...); //... Means port.
-    }
+   }
   }
 }
