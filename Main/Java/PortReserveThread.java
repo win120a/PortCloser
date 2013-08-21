6b2753283db4d6fba7b530810b16f3cacbe9e4b4
Andy Cheung <win120a@163.com>
2013-08-21 13:40:28 +0800

    Update PortReserveThread.java

--- Main/Java/PortReserveThread.java
+++ Main/Java/PortReserveThread.java
@@ -1,59 +1,62 @@
+
+
 //The Java Package import.
 import java.net.*;
 import java.io.*;
 
 //The public class, PortReserveThread.
 public class PortReserveThread implements Runnable{
-          Socket s;
-	  int port; // This thread's port.
-	  ServerSocket ss; // The ServerSocket.
+  Socket s;
+  int port; // This thread's port.
+  ServerSocket ss; // The ServerSocket.
+  
+  public PortReserveThread(int which){
+    port = which; // Set port.
+	try {
+      ss = new ServerSocket(port);  //Get New SS (ServerSocket) instance.
+	} catch (IOException e) {
+      System.out.println("Error,Will exit.");
+      System.exit(1);
+    }
+  }
 	  
-	  public PortReserveThread(int which){
-	    port = which; // Set port.
-	    try {
-			ss = new ServerSocket(port);  //Get New SS (ServerSocket) instance.
+  public void run(){ // Thread method body.
+    while(true){ // Unlimited Accept.
+      try{
+        s = ss.accept(); // Core at here! Reserve the port.
+	  }
+	  catch(SocketTimeoutException ste){
+		try {
+		  s.close();
 		} catch (IOException e) {
-			System.out.println("Error,Will exit.");
-			System.exit(1);
+		  System.out.println("Error,Will exit.");
+		  System.exit(1);
+		}
+      }
+	  catch(IOException ioe){
+		try {
+		  s.close();
+		} catch (IOException e) {
+		  System.out.println("Error,Will exit.");
+		  System.exit(1);
 		}
 	  }
-	  
-	  public void run(){ // Thread method body.
-	    while(true){ // Unlimited Accept.
-	      try{
-	        s = ss.accept(); // Core at here! Reserve the port.
-	      }
-	      catch(SocketTimeoutException ste){
-		    try {
-				s.close();
-			} catch (IOException e) {
-				System.out.println("Error,Will exit.");
-				System.exit(1);
-			}
-		  }
-	      catch(IOException ioe){
-		    try {
-				s.close();
-			} catch (IOException e) {
-				System.out.println("Error,Will exit.");
-				System.exit(1);
-			}
-	      }
-	      finally{
-	        try {
-				s.close();
-			} catch (IOException e) {
-				System.out.println("Error,Will exit.");
-				System.exit(1);
-			}
-	        s = null;
-	      }
-	    }
+	  finally{
+	    try {
+		  s.close();
+		} catch (IOException e) {
+		  System.out.println("Error,Will exit.");
+		  System.exit(1);
+		}
+	    s = null;
 	  }
-	  public void stopSelf(){  //Stop Procress.
-	    Thread.currentThread().interrupt();
-	  }
-	  public int getPort(){
-	    return port;
-	  }
-	}
+    }
+  }
+  
+  public void stopSelf(){  //Stop Procress.
+    Thread.currentThread().interrupt();
+  }
+  public int getPort(){
+	 return port;
+  }
+}
