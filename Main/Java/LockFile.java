8bc6ac3703262e2c1bac113e8dff1e6b4c847115
Andy Cheung <win120a@163.com>
2013-08-21 13:47:14 +0800

    Update LockFile.java

--- Main/Java/LockFile.java
+++ Main/Java/LockFile.java
@@ -1,23 +1,38 @@
 import java.io.*;
-import java.nio.*;
 import java.nio.channels.*;
 
 public class LockFile{
   private FileChannel ch;
   private FileLock lock;
+  private FileOutputStream fos;
 
   public LockFile(File f){
-    FileOutputStream fos = new FileOutputStream(f);
+    try {
+	  	 fos = new FileOutputStream(f);
+	  } catch (FileNotFoundException e) {
+	     System.out.println("Error,Will exit.");
+	     System.exit(1);
+	  }
     ch = fos.getChannel();
   }
 
   public FileLock start(){
-    lock = ch.lock();
+    try {
+		  lock = ch.lock();
+	  } catch (IOException e) {
+      System.out.println("Error,Will exit.");
+	    System.exit(1);
+	  }
     return lock;
   }
 
   public void end(){
-    lock.release();
+    try {
+		  lock.release();
+	  } catch (IOException e) {
+      System.out.println("Error,Will exit.");
+      System.exit(1);
+	  }
   }
 
   public class DataPackage{
