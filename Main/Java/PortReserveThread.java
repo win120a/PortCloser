02a8e29d9693851505e93ec0ff1796764d14c709
Andy Cheung <win120a@163.com>
2013-10-06 10:12:55 +0800

    Add UDP Support. (α version,but UDP Method are not completed.)

--- Main/Java/PortReserveThread.java
+++ Main/Java/PortReserveThread.java
@@ -1,19 +1,26 @@
-
 //The Java Package import.
 import java.net.*;
 import java.io.*;
 
 //The public class, PortReserveThread.
 public class PortReserveThread implements Runnable{
-  Socket s;
   int port; // This thread's port.
+  
+  // The TCP Settings Part.
+  Socket s; //The Socket, not for use.
   ServerSocket ss; // The ServerSocket.
+  
+  //The UDP Settings Part.
+  byte[] dpbyte = new byte[10];
+  DatagramSocket ds; // The DatagramSocket
+  DatagramPacket dp; // The DatagramPacket, not for use.
   
   public PortReserveThread(int which){
     port = which; // Set port.
-	try {
-      ss = new ServerSocket(port);  //Get New SS (ServerSocket) instance.
-	} catch (IOException e) {
+    try {
+      ss = new ServerSocket(port);  //Get New SS (ServerSocket[TCP]) instance.
+      ds = new DatagramSocket(port);  // DS (DatagramSocket[UDP]).
+    } catch (IOException e) {  //Catch the exception.
       System.out.println("Error,Will exit.");
       System.exit(1);
     }
@@ -22,33 +29,34 @@ public class PortReserveThread implements Runnable{
   public void run(){ // Thread method body.
     while(true){ // Unlimited Accept.
       try{
-        s = ss.accept(); // Core at here! Reserve the port.
-	  }
-	  catch(SocketTimeoutException ste){
-		try {
-		  s.close();
-		} catch (IOException e) {
-		  System.out.println("Error,Will exit.");
-		  System.exit(1);
-		}
-          }
-	  catch(IOException ioe){
-		try {
-		  s.close();
-		} catch (IOException e) {
-		  System.out.println("Error,Will exit.");
-		  System.exit(1);
-		}
-	  }
-	  finally{
-	    try {
-		  s.close();
-		} catch (IOException e) {
-		  System.out.println("Error,Will exit.");
-		  System.exit(1);
-		}
-	    s = null;
-	  }
+        s = ss.accept(); // Reserve TCP.
+        ds.receive(dp);
+      }
+      catch(SocketTimeoutException ste){
+        try {
+          s.close();
+	} catch (IOException e) {
+          System.out.println("Error,Will exit.");
+          System.exit(1);
+	}
+      }
+      catch(IOException ioe){
+	try {
+	  s.close();
+	} catch (IOException e) {
+	  System.out.println("Error,Will exit.");
+	  System.exit(1);
+	}
+      }
+      finally{
+	try {
+	  s.close();
+	} catch (IOException e) {
+	  System.out.println("Error,Will exit.");
+	  System.exit(1);
+	}
+        s = null;
+      }
     }
   }
   
@@ -56,6 +64,6 @@ public class PortReserveThread implements Runnable{
     Thread.currentThread().interrupt();
   }
   public int getPort(){
-	 return port;
+    return port;
   }
 }
